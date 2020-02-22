0d466613da9b415f7560bcb97adc9b08944ffc99
lebedys <dev@lebedys.com>
2020-02-22 22:51:47 +0000

    implement union for two busy lists

--- parse_cal.py
+++ parse_cal.py
@@ -1,19 +1,58 @@
 from ics import Calendar, Event
 import requests
+from datetime import datetime, time
+from itertools import product
 
-url = "https://timetable.soton.ac.uk/Feed/Index/fIHGtdhnnOuh7EhjMXQpJnRDR6epdJ7dXwgeUFEmcXRFB-aSPSEL8_ePZ17eCvDjzen3DuMZKJOOcDRzUxM3rA2"
+date_range = (datetime(2019, 9, 30, 0, 0), datetime(2019, 10, 10, 0, 0))
+time_range = (time(12, 0, 0), time(18, 0, 0))
 
-cal = Calendar(requests.get(url).text)
 
-# print(cal)
-# print(cal.events)
+def get_free_times(users):
+    cals = []
+    for user in users:
+        l = list(Calendar(requests.get(user.ical_url).text).timeline)
+        cals.append(l)
+    print(list)
 
-l = list(cal.timeline)
 
-Event()
+adomas_url = "https://timetable.soton.ac.uk/Feed/Index/fIHGtdhnnOuh7EhjMXQpJnRDR6epdJ7dXwgeUFEmcXRFB-aSPSEL8_ePZ17eCvDjzen3DuMZKJOOcDRzUxM3rA2"
+giorgio_url = "https://timetable.soton.ac.uk/Feed/Index/3F5CEtjYxzy3GoqHuz66AdH6zeQdZrrIFz8fMVBq9-iZOwA0W71GlsIbkmxtukoR36zSPs1OGxbnv5-YmyJ87g2"
 
-e = l[0]
+adomas = Calendar(requests.get(adomas_url).text)
+giorgio = Calendar(requests.get(giorgio_url).text)
+
+# l1 = list(adomas.timeline.included(date_range[0], date_range[1]))
+# l2 = list(giorgio.timeline.included(date_range[0], date_range[1]))
+l1 = list(adomas.timeline)
+l2 = list(giorgio.timeline)
+# t = list(cal.timeline.at())
+e = []
+l = [l1, l2]
+
+for i in range(len(l)):
+    e.append([])
+    for event in l[i]:
+        begin = datetime.fromtimestamp(event.begin.timestamp)
+        end = datetime.fromtimestamp(event.end.timestamp)
+        e[i].append((begin, end))
+    # print(e[i])
+
+events = []
+
+
+# union = []
+# union(e[0],e[1])
+
+def union(l1, l2):
+    return [(min(s1, s2), max(e1, e2)) for (s1, e1), (s2, e2) in product(l1, l2) if s1 < e2 and e1 > s2]
+
+
+overlap = union(e[0], e[1])
+
+for a in overlap:
+    print(a)
+
 # print(e, "\n")
 
-print(e.begin)
-print(e.name)
+# todo - cut off hours outside defined range
+# todo - cut off dates outside defined range
