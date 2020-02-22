df0f36275e83e4f9c68aae5d7cea2ad3da3bddd7
lebedys <dev@lebedys.com>
2020-02-22 23:51:25 +0000

    implement time_range

--- parse_cal.py
+++ parse_cal.py
@@ -5,16 +5,7 @@ from itertools import product
 import arrow
 
 date_range = (datetime(2019, 9, 30, 0, 0), datetime(2019, 10, 10, 0, 0))
-time_range = (time(12, 0, 0), time(18, 0, 0))
-
-
-def get_free_times(users):
-    cals = []
-    for user in users:
-        l = list(Calendar(requests.get(user.ical_url).text).timeline)
-        cals.append(l)
-    print(list)
-
+time_range = (time(13, 0), time(19, 0))
 
 adomas_url = "https://timetable.soton.ac.uk/Feed/Index/fIHGtdhnnOuh7EhjMXQpJnRDR6epdJ7dXwgeUFEmcXRFB-aSPSEL8_ePZ17eCvDjzen3DuMZKJOOcDRzUxM3rA2"
 giorgio_url = "https://timetable.soton.ac.uk/Feed/Index/3F5CEtjYxzy3GoqHuz66AdH6zeQdZrrIFz8fMVBq9-iZOwA0W71GlsIbkmxtukoR36zSPs1OGxbnv5-YmyJ87g2"
@@ -35,6 +26,23 @@ for i in range(len(l)):
     for event in l[i]:
         begin = datetime.fromtimestamp(event.begin.timestamp)
         end = datetime.fromtimestamp(event.end.timestamp)
+
+        dt_start = datetime.combine(datetime.date(begin), time_range[0])
+        # print(dt_start)
+        dt_end = datetime.combine(datetime.date(begin), time_range[1])
+        # print(dt_end)
+
+        if begin < dt_start:
+            if end > dt_start:
+                begin = dt_start
+            else:
+                continue
+        elif end > dt_end:
+            if begin < dt_end:
+                end = dt_end
+            else:
+                break
+
         e[i].append((begin, end))
     # print(e[i])
 
