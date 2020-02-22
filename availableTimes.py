ea68c07d1b8c2f3c4a30fba368ef5171db7caf34
Ben Watkins <contact@benwtks.com>
2020-02-22 23:09:47 +0000

    Finish selectAvailableBlocks func

--- availableTimes.py
+++ availableTimes.py
@@ -2,21 +2,21 @@ import datetime
 
 # Splits available times into blocks of the right length
 def selectAvailableBlocks(timeBlocks, meetingLength):
-    allBlocks = set() # timeBlocks but in set
+    allBlocks = timeBlocks.copy()
     available = set()
 
     while allBlocks:
-        for block in allBlocks:
-            start = block[0]
-            end = block[1]
-            duration = end - start
-            allBlocks.remove(start, end)
-            
-            if (duration >= meetingLength):
-                available.add((start, start + meetingLength))
+        block = allBlocks.pop()
 
-                if (duration > meetingLength):
-                    allBlocks.add(start - meetingLength, end)
+        start = block[0]
+        end = block[1]
+        duration = end - start
+        
+        if (duration >= meetingLength):
+            available.add((start, start + meetingLength))
+
+            if (duration > meetingLength):
+                allBlocks.append((start + meetingLength, end))
 
 
     return available
