0c7a15afa352197a3239a8ffbf763310178329ff
lebedys <dev@lebedys.com>
2020-02-23 01:16:34 +0000

    start tidying up

--- parse_cal.py
+++ parse_cal.py
@@ -4,8 +4,41 @@ from datetime import datetime, time
 from itertools import product
 import arrow
 
-date_range = (datetime(2019, 9, 30, 0, 0), datetime(2019, 10, 10, 0, 0))
-time_range = (time(6, 0), time(19, 0))
+date_range = (datetime(2019, 9, 30, 0, 0), datetime(2019, 10, 10, 0, 0)) # hard-coded test date range
+time_range = (time(6, 0), time(19, 0)) # hard-coded test time range
+
+def union(l1, l2):
+    return [(min(s1, s2), max(e1, e2)) for (s1, e1), (s2, e2) in product(l1, l2) if s1 < e2 and e1 > s2]
+
+def get_available_blocks(users, date_range, time_range):
+    cals = [] # list of calendars from iCal links
+    for user in users:
+        cals.append(Calendar(requests.get(user.ical_url).text).timeline.included(arrow.get(date_range[0]), arrow.get(date_range[1]))) # extract only dates within date_range defined in params
+
+    for i, cal in enumerate(cals):
+        blocks.append([])
+        for event in cal:
+            begin = datetime.fromtimestamp(event.begin.timestamp)
+            end = datetime.fromtimestamp(event.end.timestamp)
+
+            dt_start = datetime.combine(datetime.date(begin), time_range[0])
+            dt_end = datetime.combine(datetime.date(begin), time_range[1])
+
+            if begin < dt_start:
+                if end > dt_start:
+                    begin = dt_start
+                else:
+                    continue
+            elif end > dt_end:
+                if begin < dt_end:
+                    end = dt_end
+                else:
+                    break
+
+            blocks[i].append((begin, end))
+
+
+# test below:
 
 adomas_url = "https://timetable.soton.ac.uk/Feed/Index/fIHGtdhnnOuh7EhjMXQpJnRDR6epdJ7dXwgeUFEmcXRFB-aSPSEL8_ePZ17eCvDjzen3DuMZKJOOcDRzUxM3rA2"
 giorgio_url = "https://timetable.soton.ac.uk/Feed/Index/3F5CEtjYxzy3GoqHuz66AdH6zeQdZrrIFz8fMVBq9-iZOwA0W71GlsIbkmxtukoR36zSPs1OGxbnv5-YmyJ87g2"
@@ -13,17 +46,15 @@ giorgio_url = "https://timetable.soton.ac.uk/Feed/Index/3F5CEtjYxzy3GoqHuz66AdH6
 adomas = Calendar(requests.get(adomas_url).text)
 giorgio = Calendar(requests.get(giorgio_url).text)
 
-l1 = list(adomas.timeline.included(arrow.get(date_range[0]), arrow.get(date_range[1])))
-l2 = list(giorgio.timeline.included(arrow.get(date_range[0]), arrow.get(date_range[1])))
-# l1 = list(adomas.timeline)
-# l2 = list(giorgio.timeline)
-# t = list(cal.timeline.at())
-e = []
-l = [l1, l2]
+cal1 = list(adomas.timeline.included(arrow.get(date_range[0]), arrow.get(date_range[1])))
+cal2 = list(giorgio.timeline.included(arrow.get(date_range[0]), arrow.get(date_range[1])))
 
-for i in range(len(l)):
-    e.append([])
-    for event in l[i]:
+blocks = []
+cals = [cal1, cal2]
+
+for i, cal in enumerate(cals):
+    blocks.append([])
+    for event in cal:
         begin = datetime.fromtimestamp(event.begin.timestamp)
         end = datetime.fromtimestamp(event.end.timestamp)
 
@@ -43,20 +74,11 @@ for i in range(len(l)):
             else:
                 break
 
-        e[i].append((begin, end))
+        blocks[i].append((begin, end))
     # print(e[i])
 
-events = []
 
-
-# union = []
-# union(e[0],e[1])
-
-def union(l1, l2):
-    return [(min(s1, s2), max(e1, e2)) for (s1, e1), (s2, e2) in product(l1, l2) if s1 < e2 and e1 > s2]
-
-
-overlap = union(e[0], e[1])
+overlap = union(blocks[0], blocks[1])
 
 for a in overlap:
     print(a)
